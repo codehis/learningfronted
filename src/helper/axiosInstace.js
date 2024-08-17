daa7b027fbca92d77b81c82b606413a3fc4c520c
Mohammad Kareem <126200282+codehis@users.noreply.github.com>
2024-08-17 13:20:40 +0530

    Update axiosInstace.js

--- src/helper/axiosInstace.js
+++ src/helper/axiosInstace.js
@@ -5,8 +5,13 @@ const BASE_URL = "https://learningbackend-gugw.onrender.com/";
 const axiosInstance = axios.create({
     headers: {
         
-        "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+        headers: {
+        'Content-Type': 'application/json', // Ensure the content type is allowed by CORS policy
+        'Authorization': 'Bearer YOUR_TOKEN', // If you're using authentication
+        'X-Custom-Header': 'CustomHeaderValue', // Replace or remove this as needed
+      },
+      withCredentials: true, // Include credentials (cookies, etc.) if the API supports it);
+    })
     }
 });
 
